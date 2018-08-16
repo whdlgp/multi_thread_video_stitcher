8e0d049b7c84615ea2eac35d6cc704b77ab2467d
whdlgp <whdlgp@gmail.com>
2018-08-16 15:06:09 +0900

    Fix Stitcher setup part

--- main.cpp
+++ main.cpp
@@ -29,6 +29,9 @@ using namespace moodycamel;
 // Option for CUDA implementation
 bool try_gpu = true;
 
+// Option for Optical flow motion compensation
+#define USE_OPT_FLOW false
+
 // Option for video output
 #define VIDEO_OUT true
 #if VIDEO_OUT == true
@@ -62,12 +65,24 @@ BlockingReaderWriterQueue<thread_args>      th_arg[NUMBER_OF_THREAD];
 // Stitcher thread's output queue structure queue
 BlockingReaderWriterQueue<thread_output>    th_out[NUMBER_OF_THREAD];
 
-// Stitcher module implementation
-// create stitcher instance, setup, stitch frames
-Mat stitching(vector<Mat> &imgs)
+//OpenCV Mat handle util
+void clear_vector_mat(vector<Mat> &vec_mat)
+{
+    for(int i = 0; i < vec_mat.size(); i++)
+        vec_mat[i].release();
+    vec_mat = vector<Mat>();
+}
+
+void deep_copy_vector_mat(vector<Mat> &src, vector<Mat> &dst)
+{
+    clear_vector_mat(dst);
+    for(int i = 0; i < src.size(); i++)
+        dst.push_back(src[i].clone());
+}
+
+Ptr<Stitcher_mod> stitcher_setup()
 {
     START_TIME(Create_stitcher);
-    Mat output;
     Ptr<Stitcher_mod> stitcher = Stitcher_mod::create(Stitcher_mod::PANORAMA, try_gpu);
     STOP_TIME(Create_stitcher);
 
@@ -109,61 +124,35 @@ Mat stitching(vector<Mat> &imgs)
     }
     STOP_TIME(Setup_stitcher_modules);
 
+    return stitcher;
+}
+
+#if USE_OPT_FLOW == true
+
+#else
+// Stitcher module implementation
+// create stitcher instance, setup, stitch frames
+Mat stitching(vector<Mat> &imgs)
+{
+    Ptr<Stitcher_mod> stitcher = stitcher_setup();
+
     START_TIME(Stitch_Time);
+    Mat output;
     Stitcher_mod::Status status = stitcher->stitch(imgs, output);
     STOP_TIME(Stitch_Time);
 
     return output;
 }
+#endif
 
 #if STITCHER_DEBUG_IMWRTIE == true
 //return debug matrix with output panorama
 Mat stitching(vector<Mat> &imgs, vector<Mat> &debug_mat)
 {
-	START_TIME(Create_stitcher);
-	Mat output;
-	Ptr<Stitcher_mod> stitcher = Stitcher_mod::create(Stitcher_mod::PANORAMA, try_gpu);
-	STOP_TIME(Create_stitcher);
-
-	START_TIME(Setup_stitcher_params);
-	stitcher->setRegistrationResol(0.5);
-	stitcher->setSeamEstimationResol(0.1);
-	stitcher->setCompositingResol(Stitcher_mod::ORIG_RESOL);
-	stitcher->setPanoConfidenceThresh(1.0);
-	stitcher->setWaveCorrection(true);
-	stitcher->setWaveCorrectKind(detail::WAVE_CORRECT_HORIZ);
-	STOP_TIME(Setup_stitcher_params);
-
-	START_TIME(Setup_stitcher_modules);
-	if (try_gpu)
-	{
-#if defined(HAVE_OPENCV_XFEATURES2D) && defined(HAVE_OPENCV_CUDALEGACY)
-		stitcher->setFeaturesFinder(makePtr<detail::SurfFeaturesFinderGpu>());		//GPU
-#endif
-		stitcher->setFeaturesMatcher(makePtr<detail::BestOf2NearestMatcher>(true));
-		stitcher->setBundleAdjuster(makePtr<detail::BundleAdjusterRay>());
-#ifdef HAVE_OPENCV_CUDAWARPING
-		stitcher->setWarper(makePtr<SphericalWarperGpu>());							//GPU
-#endif	
-		stitcher->setExposureCompensator(makePtr<detail::BlocksGainCompensator>());
-		stitcher->setSeamFinder(makePtr<detail::VoronoiSeamFinder>());
-#if defined(HAVE_OPENCV_CUDAARITHM) && defined(HAVE_OPENCV_CUDAWARPING)
-		stitcher->setBlender(makePtr<detail::MultiBandBlender>(true));				//GPU
-#endif	
-	}
-	else
-	{
-		stitcher->setFeaturesFinder(makePtr<detail::OrbFeaturesFinder>());
-		stitcher->setFeaturesMatcher(makePtr<detail::BestOf2NearestMatcher>(false));
-		stitcher->setBundleAdjuster(makePtr<detail::BundleAdjusterRay>());
-		stitcher->setWarper(makePtr<SphericalWarper>());
-		stitcher->setExposureCompensator(makePtr<detail::BlocksGainCompensator>());
-		stitcher->setSeamFinder(makePtr<detail::VoronoiSeamFinder>());
-		stitcher->setBlender(makePtr<detail::MultiBandBlender>(false));
-	}
-	STOP_TIME(Setup_stitcher_modules);
+    Ptr<Stitcher_mod> stitcher = stitcher_setup();
 
 	START_TIME(Stitch_Time);
+    Mat output;
 	Stitcher_mod::Status status = stitcher->stitch(imgs, output);
 	STOP_TIME(Stitch_Time);
 
